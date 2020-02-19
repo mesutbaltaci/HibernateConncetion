a3fdc50b4f4afbbc6826a1c60d53fd84b3a37ec4
mesutbaltaci <mesutbaltaci@hotmail.com>
2020-02-19 14:04:03 -0500

    creating sessionfactory and session save object to the database

--- SF-04HibernateJava/src/org/ms/entities/Student.java
+++ SF-04HibernateJava/src/org/ms/entities/Student.java
@@ -2,6 +2,8 @@ package org.ms.entities;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -9,6 +11,7 @@ import javax.persistence.Table;
 @Table(name="student")
 public class Student {
 	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	@Column(name="id")
 	private int id;
 	
